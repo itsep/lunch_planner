1c592152265e9e9486eb211df38a7bf98c985fe6
SeJV <sebastian1210@web.de>
2018-04-05 13:02:10 +0200

    MariaDB driver works

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,16 @@
 const mariadb = require('./database_driver/mariadb_driver')
 
-mariadb.acquireConnection()
+mariadb.getConnection((err, conn)=> {
+    if (err)
+        return console.error(400)
+    conn.query('SELECT * FROM account', null, (err, rows)=> {
+        if(err) {
+            conn.release();
+            return console.error(400)
+        }
+        console.log(rows)
+        conn.release()
+    })
+})
 
-mariadb.releaseConnection()
+
