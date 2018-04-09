bb09f972194ddb5c7fe24dd329ab5a9b8750b99e
Marc <marcmehrer@t-online.de>
2018-04-09 13:36:59 +0200

    finished create-db

--- backend/bin/create-db.js
+++ backend/bin/create-db.js
@@ -1,5 +1,8 @@
 require('dotenv').load()
-const mysql = require('mysql')
+const fs = require('fs-nextra')
+const mysql = require('mysql2/promise')
+
+const path = '../database/schema.sql'
 
 
 function makeid() {
@@ -13,25 +16,54 @@ function makeid() {
 
   return text
 }
-const databaseName = makeid()
+
+async function getConnection() {
+  const conn = await mysql.createConnection({
+    host: 'localhost',
+    user: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    multipleStatements: 'true',
+  })
+  return conn
+}
+
+async function createDatabase(conn, databaseName) {
+  await conn.query('CREATE DATABASE ??', [databaseName])
+  await conn.query('USE ??', [databaseName])
+}
+
+async function importSchema(conn) {
+  const schema = await fs.readFile(path)
+  await conn.query(schema.toString())
+}
+
+async function createDBAndImportSchema(conn) {
+  const databaseName = makeid()
+  return createDatabase(conn, databaseName)
+    .then(() => importSchema(conn))
+    .then(() => databaseName)
+    .catch((error) => {
+      if (error.sqlState === 'HY000') {
+        return createDBAndImportSchema(conn)
+      }
+      throw error
+    })
+}
 
 if (!process.env.DATABASE_USERNAME || !process.env.DATABASE_PASSWORD) {
   console.error('No database username or no database password given.')
-} else {
-  module.exports = {}
-  module.exports.pool = mysql.createPool({
-    host: 'localhost',
-    user: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: databaseName,
-  })
-  module.exports.createMultiStatementConnection = () => mysql.createConnection({
-    host: 'localhost',
-    user: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: databaseName,
-    multipleStatements: 'true',
-  })
+  process.exit(1)
 }
 
-console.log(databaseName)
+async function createTestDatabase() {
+  const conn = await getConnection()
+  const dbName = await createDBAndImportSchema(conn)
+  await conn.end()
+  return dbName
+}
+
+createTestDatabase().catch((error) => {
+  console.error(error)
+  process.exit(1)
+})
+
