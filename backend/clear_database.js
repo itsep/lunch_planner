6a398b3fa2121280326ff88693c35df12da76b20
David Nadoba <dnadoba@gmail.com>
2018-04-12 19:18:09 +0200

    add clear database script

---
+++ backend/clear_database.js
@@ -0,0 +1,25 @@
+const { createMultiStatementConnection } = require('./lib/database')
+const fs = require('fs-nextra')
+
+const dbName = 'lunch_planner'
+
+const connPromise = createMultiStatementConnection(true)
+const dbSchemaPromise = fs.readFile('../database/schema.sql')
+
+async function clearDatabase() {
+  const [conn, dbSchema] = await Promise.all([connPromise, dbSchemaPromise])
+
+  await conn.query('DROP DATABASE IF EXISTS ??', [dbName])
+  await conn.query('CREATE DATABASE ??', [dbName])
+  await conn.query('USE ??', [dbName])
+  await conn.execute(dbSchema.toString())
+  return conn
+}
+
+clearDatabase()
+  .then(conn => conn.end())
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
+
