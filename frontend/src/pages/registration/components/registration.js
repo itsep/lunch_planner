52bb7e7c07bd69722455ea17956cee1fef7d7d89
David Nadoba <dnadoba@gmail.com>
2018-04-16 16:57:10 +0200

    first registration ui

--- frontend/src/pages/registration/components/registration.js
+++ frontend/src/pages/registration/components/registration.js
@@ -5,26 +5,40 @@ import TextField from 'material-ui/TextField'
 import Card, { CardActions, CardContent } from 'material-ui/Card'
 import Button from 'material-ui/Button'
 import Typography from 'material-ui/Typography'
+import Fade from 'material-ui/transitions/Fade'
+import { CircularProgress } from 'material-ui/Progress'
 
-const styles = theme => ({
+const styles = () => ({
+  registrationContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh',
+  },
   card: {
     minWidth: 275,
     maxWidth: 400,
-    marginLeft: 'auto',
-    marginRight: 'auto',
   },
   textField: {
     width: '100%',
   },
   actions: {
+    justifyContent: 'space-between',
     flexDirection: 'row-reverse',
-  }
+  },
 })
 
 class Registration extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      email: '',
+      password: '',
+      isLoading: false,
+      error: null,
+    }
+
+    this.register = this.register.bind(this)
   }
   handleChange(name) {
     const that = this
@@ -34,36 +48,76 @@ class Registration extends React.Component {
       })
     }
   }
+  register() {
+    const { email, password } = this.state
+    const data = { email, password }
+    this.setState({
+      isLoading: true,
+      error: null,
+    })
+    fetch('/api/account/register', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    }).then(response => response.json())
+      .catch((error) => {
+        this.setState({ error })
+      })
+      .finally(() => {
+        this.setState({ isLoading: false })
+      })
+  }
 
   render() {
     const { classes } = this.props
     return (
-      <div>
+      <div className={classes.registrationContainer}>
         <Card className={classes.card}>
-          <CardContent>
-            <Typography className={classes.title} variant="title">
-              Registration
-            </Typography>
-            <form className={classes.container} noValidate autoComplete="off">
+          <form className={classes.container} noValidate autoComplete="off">
+            <CardContent>
+              <Typography className={classes.title} variant="title">
+                Registration
+              </Typography>
               <TextField
                 id="email"
                 label="Email"
+                value={this.state.email}
+                onChange={this.handleChange('email')}
                 className={classes.textField}
+                disabled={this.state.isLoading}
                 margin="normal"
               />
               <TextField
                 id="password"
                 label="Password"
+                value={this.state.password}
+                onChange={this.handleChange('password')}
                 className={classes.textField}
+                disabled={this.state.isLoading}
                 type="password"
                 autoComplete="current-password"
                 margin="normal"
               />
-            </form>
-          </CardContent>
-          <CardActions className={classes.actions}>
-            <Button size="large" color="primary">Register</Button>
-          </CardActions>
+              {this.state.error &&
+                <Typography>
+                  {this.state.error.message}
+                </Typography>
+              }
+            </CardContent>
+            <CardActions className={classes.actions}>
+              <Button size="large" type="submit" color="primary" onClick={this.register} disabled={this.state.isLoading}>
+                Register
+              </Button>
+              <Fade
+                in={this.state.isLoading}
+                unmountOnExit
+              >
+                <CircularProgress />
+              </Fade>
+            </CardActions>
+          </form>
         </Card>
       </div>
     )
