38bc564ff670825379d2f2d5d23a4a3cf609411e
David Nadoba <dnadoba@gmail.com>
2018-04-23 13:34:29 +0200

    make registration app name camelcase

--- frontend/src/pages/registration/components/registration_app.js
+++ frontend/src/pages/registration/components/registration_app.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import Registration from './registration'
 
-function Registration_app() {
+function RegistrationApp() {
   return (
     <Registration />
   )
 }
 
-export default Registration_app
+export default RegistrationApp
--- frontend/src/pages/registration/registration.js
+++ frontend/src/pages/registration/registration.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import Registration_app from './components/registration_app'
+import RegistrationApp from './components/registration_app'
 import '../../style/main.scss'
 
-render(<Registration_app />, document.getElementById('app'))
+render(<RegistrationApp />, document.getElementById('app'))
