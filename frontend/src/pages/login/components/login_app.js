181ba76638666120cdd1a7660538f61f6eb7021b
David Nadoba <dnadoba@gmail.com>
2018-04-23 10:01:42 +0200

    add login page

---
+++ frontend/src/pages/login/components/login_app.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Registration from './login'
+
+function Login_app() {
+  return (
+    <Registration />
+  )
+}
+
+export default Login_app
---
+++ frontend/src/pages/login/login.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { render } from 'react-dom'
+import Login_app from './components/login_app'
+import '../../style/main.scss'
+
+render(<Login_app />, document.getElementById('app'))
