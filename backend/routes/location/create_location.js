6e4784a09794f7a585772f66d5601330ef25c5ac
Marc <marcmehrer@t-online.de>
2018-05-07 15:25:01 +0200

    create location "fertig" Berechtigung wird nicht geprüft, daher noch keine tests

--- backend/routes/location/create_location.js
+++ backend/routes/location/create_location.js
@@ -1,5 +1,8 @@
 const { pool } = require('../../lib/database')
-const { validLength } =require('../../lib/authenti')
+const { validLength } = require('../../lib/validation')
+
+const minimumLength = 1
+const maximumLength = 64
 
 async function create(name, coordinates, lunchspaceId) {
   await pool.execute('INSERT INTO location (name, coordinates, lunchspace_id) ' +
@@ -8,13 +11,22 @@ async function create(name, coordinates, lunchspaceId) {
 }
 
 async function createLocation(req, res) {
-  const { name, coordinates, lunchspaceId } = req.body
+  const { userId } = req.token
+  const { coordinates, lunchspaceId } = req.body
+  let { name } = req.body
+  if (!validLength(name, maximumLength, minimumLength)) {
+    return res.status(409).json({ error: 'Name must be between 1 and 64 characters long.' })
+  }
+  name = name.trim()
+  try {
+    await pool.execute('SELECT FROM member_of WHERE')
+  }
   try {
     await create(name, coordinates, lunchspaceId)
-    res.status(200).end()
   } catch (error) {
     throw error
   }
+  return res.status(200).end()
 }
 
 module.exports = {
--- backend/routes/location/index.js
+++ backend/routes/location/index.js
@@ -2,11 +2,12 @@ const { Router } = require('express')
 const asyncHandler = require('express-async-handler')
 const bodyParser = require('body-parser')
 const { createLocation } = require('./create_location')
+const { authenticate } = require('../../middleware/authenticate')
 
 const locationRouter = Router()
 locationRouter.use(bodyParser.json())
 
-locationRouter.post('/', asyncHandler(createLocation()))
+locationRouter.post('/', authenticate, asyncHandler(createLocation()))
 
 module.exports = {
   router: locationRouter,
