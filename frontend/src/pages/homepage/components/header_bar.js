ef8eaa79d687f966b3f32370aaab98cb43e2769f
murmelmunz <munzinger3@gmail.com>
2018-05-09 15:47:40 +0200

    style angepasst

--- frontend/src/pages/homepage/components/header_bar.js
+++ frontend/src/pages/homepage/components/header_bar.js
@@ -2,30 +2,62 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { AppBar, Toolbar, Avatar, Typography, Button } from 'material-ui'
+import { withStyles } from 'material-ui/styles'
+
+const styles = () => ({
+  flexContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    backgroundColor: '#75a045',
+  },
+  logo: {
+    backgroundColor: 'grey',
+    color: 'black',
+  },
+  lunchspace: {
+    color: 'black',
+  },
+  user: {
+    display: 'flex',
+    flexShrink: 0,
+  },
+  avatar: {
+    // width: '15%',
+  },
+  profileName: {
+    marginLeft: '10px',
+    color: 'black',
+  },
+  logout: {
+    color: 'black',
+  },
+  menuButton: {
+    marginLeft: 12,
+    marginRight: 20,
+  },
+})
 
 const mapStateToProps = state => ({
   profile: state.profile,
   lunchspace: state.lunchspace,
 })
 
-function HeaderBar({ profile, lunchspace }) {
+function HeaderBar({ classes, profile, lunchspace }) {
   return (
     <div>
-      <AppBar position="static" color="default">
-        <Toolbar>
-          <Avatar
-            src={profile.src}
-          />
-          <Typography color="inherit">
-            {profile.firstName}
-            {profile.lastName}
-          </Typography>
-          <Typography variant="title" color="inherit">
+      <AppBar className={classes.appBar} position="static" color="default">
+        <Toolbar className={classes.flexContainer}>
+          <Button className={classes.logo} variant="raised" color="secondary" float="right">
+              LOGO VSF
+          </Button>
+          <Typography className={classes.lunchspace} variant="title" color="inherit">
             {lunchspace.name}
           </Typography>
-          <Button variant="raised" color="secondary">
-            logout
-          </Button>
+          <div className={classes.user}>
+            <Button className={classes.logout} >
+              <Avatar alt="David Nadoba" className={classes.avatar} src={profile.src} />
+            </Button>
+          </div>
         </Toolbar>
       </AppBar>
     </div>
@@ -42,6 +74,7 @@ HeaderBar.propTypes = {
     name: PropTypes.string.isRequired,
     subdomain: PropTypes.string.isRequired,
   }).isRequired,
+  classes: PropTypes.object.isRequired,
 }
 
-export default connect(mapStateToProps)(HeaderBar)
+export default withStyles(styles)(connect(mapStateToProps)(HeaderBar))
