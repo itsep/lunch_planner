0bbc60adc99f8683d4570210afbf64bd7bdacde7
SeJV <sebastian1210@web.de>
2018-05-10 13:32:46 +0200

    get Location test

--- backend/__tests__/test_location/test_get_locations.js
+++ backend/__tests__/test_location/test_get_locations.js
@@ -1,16 +1,38 @@
 const { getLocations } = require('../../routes/location/get_locations')
+const { createLocation } = require('../../routes/location/create_location')
+const { createLunchspace } = require('../../routes/lunchspace/create_lunchspace')
 const { createMockDatabase, dropMockDatabase } = require('../../lib/database/mock')
 const { mockReq, mockRes } = require('../../lib/express_mock')
-const { pool } = require('../../lib/database')
 
+const testSpaceName = 'testspace'
+const testSpaceSubdomain = 'test-space-subdomain'
+const testLocationName = 'McBurger'
+const testLocationCoordinates = { lat: 20, long: 10 }
 
 describe('get locations', () => {
   beforeAll(createMockDatabase)
   afterAll(dropMockDatabase)
-  // TODO: create Lunchspace und create Locations
-  beforeAll()
+  beforeAll(async () => {
+    let req = mockReq({
+      body: { lunchspaceName: testSpaceName, lunchspaceSubdomain: testSpaceSubdomain },
+      token: { userId: 1 },
+    })
+    let res = mockRes()
+    await createLunchspace(req, res)
+    req = mockReq({
+      body: {
+        coordinates: testLocationCoordinates,
+        name: testLocationName,
+      },
+      lunchspace: { id: 1 },
+    })
+    res = mockRes()
+    await createLocation(req, res)
+  })
   it('should result Locations', () => {
-    const req = mockReq()
+    const req = mockReq({
+      lunchspace: 1,
+    })
     const res = mockRes()
     getLocations(req, res)
     console.log('INSERT TEST!')
--- backend/routes/location/get_locations.js
+++ backend/routes/location/get_locations.js
@@ -3,5 +3,8 @@ const { pool } = require('../../lib/database')
 async function getLocations(req, res) {
   const { id } = req.lunchspace
   const [result] = await pool.execute('SELECT join_up_at.*, location.name, location.coordinates FROM join_up_at LEFT JOIN location ON join_up_at.location_id = location.id WHERE location.lunchspace_id = ?', [id])
+}
 
+module.exports = {
+  getLocations,
 }
