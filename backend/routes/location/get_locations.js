e4a6fd3c46c362548bfe6fdb19e69d93cdb67226
Marc <marcmehrer@t-online.de>
2018-05-11 14:54:03 +0200

    mapped locations to show x and y as lat and long; removed console.log from database/index

--- backend/routes/location/get_locations.js
+++ backend/routes/location/get_locations.js
@@ -3,6 +3,12 @@ const { pool } = require('../../lib/database')
 async function getLocationsAndParticipants(id) {
   return pool.useConnection(async (conn) => {
     const [locations] = await conn.execute('SELECT * FROM location WHERE lunchspace_id = ?', [id])
+    locations.map((element) => {
+      const location = element
+      location.coordinates = { lat: location.coordinates.x, long: location.coordinates.y }
+      return true
+    })
+    console.log(locations[0].coordinates)
     const [participants] = await conn.execute('SELECT * FROM event_participants WHERE lunchspace_id = ?', [id])
     const result = { locations, participants }
     return result
