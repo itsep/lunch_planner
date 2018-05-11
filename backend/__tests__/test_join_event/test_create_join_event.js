6c5f0d3e7a8aea84b89f9945aaccb09f4f9e9c0c
SeJV <sebastian1210@web.de>
2018-05-11 16:47:26 +0200

    create Join Event tests erstellt

---
+++ backend/__tests__/test_join_event/test_create_join_event.js
@@ -0,0 +1,36 @@
+const { create } = require('../../routes/join_event/create_join_event')
+const location = require('../../routes/location/create_location')
+const account = require('../../routes/account/register_account')
+const { createLunchspace } = require('../../routes/lunchspace/create_lunchspace')
+const { createMockDatabase, dropMockDatabase } = require('../../lib/database/mock')
+
+const testSpaceName = 'testspace'
+const testSpaceSubdomain = 'test-space-subdomain'
+const testLocationName = 'McBurger'
+const testLocationCoordinates = { lat: 20, long: 10 }
+const testFirstName = 'Max'
+const testLastName = 'Mustermann'
+const testEmail = 'max.mustermann@gmail.com'
+const testPassword = 'password'
+const testTime = '10:30'
+const testDate = '2018-10-12'
+
+let testUserId
+let testLunchspaceId
+let testLocationId
+
+describe('create join event', () => {
+  beforeAll(createMockDatabase)
+  afterAll(dropMockDatabase)
+  beforeAll(async () => {
+    testUserId = await account.create(testEmail, testPassword, testFirstName, testLastName)
+    testLunchspaceId = await createLunchspace(testUserId, testSpaceName, testSpaceSubdomain)
+    testLocationId = await location
+      .create(testLocationName, testLocationCoordinates, testLunchspaceId)
+  })
+  describe('create', async () => {
+    it('should create a location in DB', async () => {
+      await create(testUserId, testLocationId, testTime, testDate)
+    })
+  })
+})
--- backend/routes/join_event/create_join_event.js
+++ backend/routes/join_event/create_join_event.js
@@ -1,18 +1,33 @@
 const { pool } = require('../../lib/database')
 
 async function create(userId, locationId, eventTime, eventDate) {
-  const [result] = await pool.execute('INSERT INTO join_up_at (user_id, location_id, event_time, event_date) ' +
-    'VALUES (?, ?, TIME(?:?), DATE(?-?-?)', [userId, locationId, eventTime.hour, eventTime.minute, eventDate.year, eventDate.month, eventDate.day])
-  return result.insertId
+  await pool.execute('INSERT INTO join_up_at (user_id, location_id, event_time, event_date) ' +
+    'VALUES (?, ?, ?, ?)', [userId, locationId, eventTime, eventDate])
 }
 
+function timeForSQL(time) {
+  const eventTimeHour = time.hour.toString().length === 2 ? time.hour : `0${time.hour}`
+  const eventTimeMinute = time.minute.toString().length === 2 ? time.minute : `0${time.minute}`
+  return `${eventTimeHour}:${eventTimeMinute}`
+}
+
+function dateForSQL(date) {
+  const eventDateYear = date.year.toString()
+  const eventDateMonth = date.month.toString()
+  const eventDateDay = date.day.toString()
+  return `${eventDateYear}-${eventDateMonth}-${eventDateDay}`
+}
 async function createJoinEvent(req, res) {
   const {
     userId,
     locationId,
+  } = req.body
+  let {
     eventTime,
     eventDate,
   } = req.body
+  eventTime = timeForSQL(eventTime)
+  eventDate = dateForSQL(eventDate)
   await create(userId, locationId, eventTime, eventDate)
   return res.status(200).end()
 }
