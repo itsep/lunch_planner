a04f248782c3ea80d0acb673b35dfc1ed21dbd25
Marc <marcmehrer@t-online.de>
2018-05-14 09:57:14 +0200

    renamed response parameters of getLocation to suite the parameters in requests

--- backend/routes/location/get_locations.js
+++ backend/routes/location/get_locations.js
@@ -2,13 +2,16 @@ const { pool } = require('../../lib/database')
 
 async function getLocationsAndParticipants(id) {
   return pool.useConnection(async (conn) => {
-    const [locations] = await conn.execute('SELECT * FROM location WHERE lunchspace_id = ?', [id])
+    const [locations] = await conn.execute('SELECT id, name, coordinates FROM location WHERE lunchspace_id = ?', [id])
     locations.map((element) => {
       const location = element
       location.coordinates = { lat: location.coordinates.x, long: location.coordinates.y }
       return true
     })
-    const [participants] = await conn.execute('SELECT * FROM event_participants WHERE lunchspace_id = ?', [id])
+    const [participants] = await conn.execute('SELECT user_id as userId, location_id as locationId,' +
+      ' event_time as eventTime, event_date as eventDate,' +
+      ' first_name as firstName, last_name as lastName, image_url as imageUrl' +
+      ' FROM event_participants WHERE lunchspace_id = ?', [id])
     const result = { locations, participants }
     return result
   })
