cb4b4ae3764600ac85b6c51f729cccd0aeee7cf0
David Nadoba <dnadoba@gmail.com>
2018-05-28 14:00:41 +0200

    fix shared liniting

--- shared/lib/error/to_locailzable_error.test.js
+++ shared/lib/error/to_locailzable_error.test.js
@@ -2,7 +2,6 @@ const { LocalizableError } = require('./localizable_error')
 const { AuthorizationError } = require('./authorization_error')
 const { AuthenticationError } = require('./authentication_error')
 const { InputValidationError } = require('./input_validation_error')
-const { SQLError } = require('./sql_error')
 const { UnknownError } = require('./unknown_error')
 const { toLocalizableError } = require('./to_locailzable_error')
 
