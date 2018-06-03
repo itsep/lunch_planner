b06fe394b241d99d0a50b36f8638812841e8a435
David Nadoba <dnadoba@gmail.com>
2018-06-03 21:38:40 +0200

    add ChangeDispatcher

--- frontend/src/pages/homepage/homepage.js
+++ frontend/src/pages/homepage/homepage.js
@@ -2,16 +2,27 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
+import io from 'socket.io-client'
 import thunkMiddleware from 'redux-thunk'
 import stateReducer from './reducer'
 import { fetchPageData } from './actions'
+import ChangeDispatcher from './change_dispatcher'
 import HomepageApp from './components/homepage_app'
 import '../../style/main.scss'
+
+const socket = io({
+  path: '/subscriber',
+  extraHeaders: {
+    subdomain: 'vsf-experts-ma',
+  },
+})
 
 const store = createStore(
   stateReducer,
   applyMiddleware(thunkMiddleware)
 )
+
+const subscriber = new ChangeDispatcher(socket, store)
 
 store.dispatch(fetchPageData('vsf-experts-ma', new Date()))
 
