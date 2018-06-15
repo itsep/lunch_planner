1c802bb72a0d5280d2ac998159d64768ad625abb
Marc <marcmehrer@t-online.de>
2018-06-15 15:02:16 +0200

    backend ready for frontend

--- backend/middleware/store_image_file.js
+++ backend/middleware/store_image_file.js
@@ -4,7 +4,7 @@ const { InputValidationError } = require('../../shared/lib/error')
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, './uploads/')
+    cb(null, './images/')
   },
   filename: (req, file, cb) => {
     cb(null, `${uuidv4()}.png`)
--- backend/routes/account/upload_profile_picture.js
+++ backend/routes/account/upload_profile_picture.js
@@ -1,5 +1,4 @@
 const { pool } = require('../../lib/database')
-const config = require('config')
 const fs = require('fs-nextra')
 const sharp = require('sharp')
 const uuidv4 = require('uuid/v4')
@@ -8,15 +7,34 @@ async function saveURL(userId, url) {
   await pool.execute('UPDATE user SET image_url = ? WHERE id = ?', [url, userId])
 }
 
+function getPathFromUrl(url) {
+  let path
+  if (typeof url === 'string') {
+    const parts = url.split('/')
+    path = `./${parts[2]}/${parts[3]}`
+  }
+  return path
+}
+
 async function uploadProfilePicture(req, res) {
   const { userId } = req.token
   const { imageUrl } = await req.userPromise
-  const url = `./uploads/${uuidv4()}.png`
-  await sharp(req.file.path).resize(256, 256).toFile(url)
+  const name = uuidv4()
+  const path = `./images/${name}.png`
+  const url = `/api/images/${name}.png`
+
+  const oldPath = getPathFromUrl(imageUrl)
+
+  sharp.cache(false)
+  await sharp(req.file.path).resize(256, 256).toFile(path)
+
   await saveURL(userId, url)
+
+  res.
+
   await fs.unlink(req.file.path)
-  if (fs.exists(imageUrl)) {
-    await fs.unlink(imageUrl)
+  if (await fs.exists(oldPath)) {
+    await fs.unlink(oldPath)
   }
   return res.status(200).json({})
 }
