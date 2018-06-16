68f8c08502640594993a71603096385d099e86c4
David Nadoba <dnadoba@gmail.com>
2018-06-17 00:49:47 +0200

    fix bug their joined times before 10am would not show up

--- backend/lib/formatation.js
+++ backend/lib/formatation.js
@@ -1,7 +1,7 @@
 function timeForSQL(time) {
-  const eventTimeHour = time.hour.toString().length === 2 ? time.hour : `0${time.hour}`
+  const eventTimeHour = time.hour
   const eventTimeMinute = time.minute.toString().length === 2 ? time.minute : `0${time.minute}`
-  return `${eventTimeHour}:${eventTimeMinute}`
+  return `${eventTimeHour}:${eventTimeMinute}:00`
 }
 
 function dateForSQL(date) {
