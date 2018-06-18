e7c5a2e1cf281a1bfc5d30d6547c8eefe1829b15
SeJV <sebastian1210@web.de>
2018-06-18 16:16:52 +0200

    lint fix

--- frontend/src/pages/imprint/components/imprint_app.js
+++ frontend/src/pages/imprint/components/imprint_app.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Imprint from './imprint'
 import localizedStrings from '../../../lib/localization'
 import CommonAppContainer from '../../../components/common_app_container'
 import UnauthorizedHeaderBar from '../../../components/unauthorized_header_bar'
 
-class ImprintApp extends Component {
-  render() {
-    return (
-      <CommonAppContainer>
-        <UnauthorizedHeaderBar title={localizedStrings.imprint} />
-        <Imprint />
-      </CommonAppContainer>
-    )
-  }
+function ImprintApp() {
+  return (
+    <CommonAppContainer>
+      <UnauthorizedHeaderBar title={localizedStrings.imprint} />
+      <Imprint />
+    </CommonAppContainer>
+  )
 }
 
 export default ImprintApp
